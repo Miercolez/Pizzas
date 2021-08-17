d9019ca704ecb5287ff969adedcf6f1506fbc73c
Jonas Fredriksson <48363473+Miercolez@users.noreply.github.com>
2021-08-17 14:13:33 +0200

    Added Get,Post,Put and Delete mapping

--- src/main/java/com/example/pizzas/controllers/PizzaController.java
+++ src/main/java/com/example/pizzas/controllers/PizzaController.java
@@ -4,6 +4,8 @@ import com.example.pizzas.entities.Pizza;
 import com.example.pizzas.repositories.PizzaRepository;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,18 +19,43 @@ public class PizzaController {
     }
 
     @GetMapping("/pizzas")
-    public List<Pizza> getAllPizzas(){
+    public List<Pizza> getAllPizzas() {
         return pizzaRepository.findAll();
     }
 
-    @GetMapping("/pizzas/{id}")
-    Optional<Pizza> getPizzaById(@PathVariable("id") Long id){
-        return pizzaRepository.findById(id);
+    @GetMapping("/pizzas/{name}")
+    List<Pizza> getPizzaById(@PathVariable("name") String name) {
+        HashSet pizzas = new HashSet();
+        pizzas.addAll(pizzaRepository.findPizzaByName(name));
+        pizzas.addAll(pizzaRepository.findPizzaByIngredients(name));
+
+        try {
+            pizzas.addAll(pizzaRepository.findPizzaByPrice(Integer.parseInt(name)));
+        }catch (NumberFormatException e){
+        }
+
+        return new ArrayList<>(pizzas);
+    }
+
+    @PostMapping("/pizzas")
+    Pizza addPizza(@RequestBody Pizza pizza) {
+        return pizzaRepository.save(pizza);
     }
 
     @PutMapping("/pizzas/{id}")
-    Optional<Pizza> updatePizza(@RequestBody Pizza newPizza, @PathVariable("id") Long id){
-        return pizzaRepository.findById(id);
+    Optional<Pizza> updatePizza(@RequestBody Pizza newPizza, @PathVariable("id") Long id) {
+        return pizzaRepository.findById(id)
+                .map(pizza -> {
+                    pizza.setName(newPizza.getName());
+                    pizza.setIngredients(newPizza.getIngredients());
+                    pizza.setPrice(newPizza.getPrice());
+                    return pizzaRepository.save(pizza);
+                });
+    }
+
+    @DeleteMapping("/pizzas/{id}")
+    void deletePizza(@PathVariable("id") Long id){
+        pizzaRepository.deleteById(id);
     }
 
 }
--- src/main/java/com/example/pizzas/repositories/PizzaRepository.java
+++ src/main/java/com/example/pizzas/repositories/PizzaRepository.java
@@ -3,5 +3,10 @@ package com.example.pizzas.repositories;
 import com.example.pizzas.entities.Pizza;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PizzaRepository extends JpaRepository<Pizza,Long> {
+    List<Pizza> findPizzaByName(String name);
+    List<Pizza> findPizzaByIngredients(String name);
+    List<Pizza> findPizzaByPrice(int price);
 }
