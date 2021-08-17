916715f3f0dde4175d0c640b22a0850156e2bc44
Jonas Fredriksson <48363473+Miercolez@users.noreply.github.com>
2021-08-17 14:45:16 +0200

    Added tests for adding pizza

--- src/test/java/com/example/pizzas/controllers/PizzaMVCTest.java
+++ src/test/java/com/example/pizzas/controllers/PizzaMVCTest.java
@@ -1,16 +1,21 @@
 package com.example.pizzas.controllers;
 
+import com.example.pizzas.entities.Pizza;
 import com.example.pizzas.repositories.PizzaRepository;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -28,5 +33,21 @@ class PizzaMVCTest {
     void getAllPizzasReturnsOnePizza() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/pizzas"))
                 .andExpect(status().is2xxSuccessful());
+    }
+
+    @Test
+    void addPizzaAndExceptOK() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/pizzas")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{ \"name\": \"Visuvio\", \"price\": 70 }"))
+                .andExpect(status().is2xxSuccessful());
+    }
+
+    @Test
+    void addPizzaAndExceptERROR() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/pizzas")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(""))
+                .andExpect(status().is4xxClientError());
     }
 }
